6186174389d01c27321119249236b44b37e2ebe4
ryangwsimmons <ryangwsimmons@users.noreply.github.com>
2025-06-06 01:41:07 +0000

    deploy: 26b340c94a7ca5b6fb01d54c9d2d4297ed20459b

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "WhiteNoiseLinux",
+        "excerpt":"A Linux application that plays white noise (and other ambient sound) audio files in the background. Supports layering multiple sounds over one another. Sounds automatically loop indefinitely. Installation Distro Packages (Recommended) Use the latest provided package for your distro, available from the OpenSUSE Build Service. Click the link, select your...","categories": [],
+        "tags": [],
+        "url": "/projects/whitenoiselinux/",
+        "teaser": "/assets/images/whitenoiselinux/icon.svg"
+      },{
+        "title": "WAMobile",
+        "excerpt":"An Android app to access the University of Guelph’s WebAdvisor service. With WAMobile, you can search for course sections, look at your grades, view your financial account, as well as see any announcements from the University of Guelph. Download Now Installation Download the latest APK file using the button above...","categories": [],
+        "tags": [],
+        "url": "/projects/wamobile/",
+        "teaser": "/assets/images/wamobile/icon.svg"
+      },{
+        "title": "Vim-MdToPdf",
+        "excerpt":"A plugin for the Vim text editor to convert Markdown documents to PDF files. Supports entry of math using TeX. Installation Dependencies Python 3 A version of Vim compiled with Python 3 support (run vim --version to check this, if you see +python3, you’re good) The lxml Python module The...","categories": [],
+        "tags": [],
+        "url": "/projects/vim-mdtopdf/",
+        "teaser": "/assets/images/vim-mdtopdf/icon.svg"
+      }]
